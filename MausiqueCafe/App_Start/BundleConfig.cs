0b48d31945aea418620e0ee112eabcce03975f5f
Abhishek Sharma <abhi.as001@gmail.com>
2019-09-10 13:42:04 +0530

    template added

--- MausiqueCafe/App_Start/BundleConfig.cs
+++ MausiqueCafe/App_Start/BundleConfig.cs
@@ -20,11 +20,27 @@ namespace MausiqueCafe
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Content/web/styles/bootstrap4/popper.js",
+                         "~/Content/web/styles/bootstrap4/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/otherJS").Include(
+                 "~/Content/web/plugins/OwlCarousel2-2.2.1/owl.carousel.js",
+                 "~/Content/web/plugins/parallax-js-master/parallax.min.js",
+                 "~/Content/web/js/custom.js",
+                 "~/Content/web/engine1/wowslider.js",
+                 "~/Content/web/engine1/script.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/web/styles/bootstrap4/bootstrap.min.css",
+                      "~/Content/web/styles/main_styles.css",
+                      "~/Content/web/styles/responsive.css",
+                      "~/Content/web/engine1/style.css",
+                      "~/Content/web/plugins/font-awesome-4.7.0/css/font-awesome.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/OwlCarousel2").Include(
+                    "~/Content/web/plugins/OwlCarousel2-2.2.1/owl.carousel.css",
+                    "~/Content/web/plugins/OwlCarousel2-2.2.1/owl.theme.default.css",
+                    "~/Content/web/plugins/OwlCarousel2-2.2.1/animate.css"));
         }
     }
 }
